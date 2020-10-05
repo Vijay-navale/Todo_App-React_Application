e7610f7061a15d7b4e9ecd190b39f1272917df81
Vijay-navale <vijaynavale1999@gmail.com>
2020-10-05 23:14:25 +0530

    This commmit is for pushing build folder to github because gh-pages is not working

---
+++ build/precache-manifest.065e36c8f5ebc1848bda0600dae562dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c33e2008d368b3def4074051a40292e",
+    "url": "/Todo_App-React_Application/index.html"
+  },
+  {
+    "revision": "da3176c1b66b0d9e167c",
+    "url": "/Todo_App-React_Application/static/css/main.cd5c1491.chunk.css"
+  },
+  {
+    "revision": "6d399d597b06eaa01bd7",
+    "url": "/Todo_App-React_Application/static/js/2.5d1b9ffc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo_App-React_Application/static/js/2.5d1b9ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3176c1b66b0d9e167c",
+    "url": "/Todo_App-React_Application/static/js/main.c771b473.chunk.js"
+  },
+  {
+    "revision": "aad49fa0d30c2f48557f",
+    "url": "/Todo_App-React_Application/static/js/runtime-main.d23aa3f3.js"
+  }
+]);
